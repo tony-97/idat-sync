434b8d4262edadd782b7881db1744dba2c3f8110
tony-97 <taaffaat_1@hotmail.com>
2025-10-24 12:40:33 -0500

    fix: handle None values

--- main.py
+++ main.py
@@ -266,9 +266,9 @@ class IDATSync:
         ).execute_query_retry()
         time.sleep(REQUEST_DELAY)
         for row in result.value.PrimaryQueryResult.RelevantResults.Table.Rows:
-            title: str = (row.Cells or {}).get("Title", "") or ""
-            parent_link: str = (row.Cells or {}).get("ParentLink", "") or ""
-            site_name: str = (row.Cells or {}).get("SiteName", "") or ""
+            title: str = (row.Cells or {}).get("Title") or ""
+            parent_link: str = (row.Cells or {}).get("ParentLink") or ""
+            site_name: str = (row.Cells or {}).get("SiteName") or ""
             if (
                 title.startswith(course_name)
                 and parent_link.lower().endswith("grabaciones")
